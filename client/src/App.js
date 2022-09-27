e183c6961b9a8981194bbdf400887ab736ca9eda
Matias <matimogica@gmail.com>
2022-09-27 19:49:24 -0300

    agregado about

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,44 @@
 
 /* import "./App.css"; */
 import { Route, Switch } from "react-router-dom";
-import { Home } from "./components/Home/Home";
-import ErrorPage from "./components/ErrorPage/ErrorPage";
-import Details from "./components/Details/Details.jsx";
-import Formulario from "./components/Formulario/Formulario";
-// register
-import Register from "./components/Register/Register.jsx";
-import NavBar from "./components/NavBar/NavBar.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import About from "./components/About/About.jsx";
-import ComponentProducts from "./components/ComponentProducts/ComponentProducts.jsx";
-import ComponentProductsGestion from "./components/ComponentProductsGestion/ComponentProductsGestion";
-import Contact from "./components/Contact/Contact";
-import Carry from "./components/Carry/Carry";
-// import GoogleLogin from "react-google-login";
-import Login from "./components/Login/Login";
-import payment from "./components/Pago/Pay";
-import { AuthProvider } from "./context/authContext.jsx";
-import Profile from "./components/Profile/Profile";
-import ModifyItem from "./components/ModifyITem/ModifyItem";  
-import UsersAdmin from "./components/UsersAdmin/UsersAdmin";
-import AdminOrders from "./components/AdminOrders/AdminOrders.tsx";
-import Favorites from "./components/Favs/Favorites";
-import ModifyUser from "./components/ModifyUser/ModifyUser";
-import ModifyUserImage from "./components/ModifyUser/ModifyUserImage";
-import ModifyUserPassword from "./components/ModifyUser/ModifyUserPassword";
+import About from "./Pages/About/About";
+import { Home } from "./Pages/Home";
+// import ErrorPage from "./components/ErrorPage/ErrorPage";
+// import Details from "./components/Details/Details.jsx";
+// import Formulario from "./components/Formulario/Formulario";
+// // register
+// import Register from "./components/Register/Register.jsx";
+// import NavBar from "./components/NavBar/NavBar.jsx";
+// import Footer from "./components/Footer/Footer.jsx";
+// import About from "./Pages/Home/About/About.jsx";
+// import ComponentProducts from "./components/ComponentProducts/ComponentProducts.jsx";
+// import ComponentProductsGestion from "./components/ComponentProductsGestion/ComponentProductsGestion";
+// import Contact from "./components/Contact/Contact";
+// import Carry from "./components/Carry/Carry";
+// // import GoogleLogin from "react-google-login";
+// import Login from "./components/Login/Login";
+// import payment from "./components/Pago/Pay";
+// import { AuthProvider } from "./context/authContext.jsx";
+// import Profile from "./components/Profile/Profile";
+// import ModifyItem from "./components/ModifyITem/ModifyItem";  
+// import UsersAdmin from "./components/UsersAdmin/UsersAdmin";
+// import AdminOrders from "./components/AdminOrders/AdminOrders.tsx";
+// import Favorites from "./components/Favs/Favorites";
+// import ModifyUser from "./components/ModifyUser/ModifyUser";
+// import ModifyUserImage from "./components/ModifyUser/ModifyUserImage";
+// import ModifyUserPassword from "./components/ModifyUser/ModifyUserPassword";
 
-import NavUser from "./components/NavUser/NavUser.jsx";
-import Orders2 from "./components/Orders/Orders2";
-import OrdersDetails from "./components/Orders/OrdersDetails";
+// import NavUser from "./components/NavUser/NavUser.jsx";
+// import Orders2 from "./components/Orders/Orders2";
+// import OrdersDetails from "./components/Orders/OrdersDetails";
 
-// login Google
-import LoginGoogle from "./components/Login/Login Google/LoginGoogle";
+// // login Google
+// import LoginGoogle from "./components/Login/Login Google/LoginGoogle";
 
-import styles from "./App.module.css";
-import AdminDetailOrder from "./components/AdminOrders/AdminDetailOrder.jsx";
-import FormDelivery from "./components/PasarelaDePago/FormularioContactoDelivery"
-import MethodPay from "./components/PasarelaDePago/ResumenMetododeCompra"
+// import styles from "./App.module.css";
+// import AdminDetailOrder from "./components/AdminOrders/AdminDetailOrder.jsx";
+// import FormDelivery from "./components/PasarelaDePago/FormularioContactoDelivery"
+// import MethodPay from "./components/PasarelaDePago/ResumenMetododeCompra"
 
 function App() {
 
@@ -46,25 +47,28 @@ function App() {
     <div className={styles.App}>
       <AuthProvider>
         {/* nav bar */}
-        <NavBar />
+        {/* <NavBar /> */}
         <Switch>
           <Route exact path="/">
             <Home />{" "}
           </Route>
-          <Route exact path="/gestionProducts">
+          <Route exact path="/about">
+            <About />{" "}
+          </Route>
+          {/* <Route exact path="/gestionProducts">
             <ComponentProductsGestion />{" "}
             {/*Se pone asi porque los componentes estan creadas como Clase*/}
-          </Route>
+          {/* </Route>
           <Route exact path="/products/:gender">
-            <ComponentProducts />{" "}
+            <ComponentProducts />{" "} */}
             {/*Se pone asi porque los componentes estan creadas como Clase*/}
-          </Route>
+          {/* </Route>
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/login" component={Login} />
           <Route path="/details/:id" component={Details}></Route>{" "}
-          <Route exact path="/createProduct" component={Formulario} />{" "}
+          <Route exact path="/createProduct" component={Formulario} />{" "} */}
           {/*Se pone asi porque los componentes estan creadas como Funcion*/}
-          <Route  exact path="/about" component={About}></Route>
+          {/* <Route  exact path="/about" component={About}></Route>
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/LoginGoogle" component={LoginGoogle} />
           <Route exact path="/register" component={Register} />
@@ -86,7 +90,7 @@ function App() {
           <Route exact path="/usersAdmin" component={UsersAdmin} />  
          
           <Route exact path="/modifyProduct/:id" component={ModifyItem} />  
-          <Route component={ErrorPage}></Route>
+          <Route component={ErrorPage}></Route>  */}
           
 
 
@@ -95,7 +99,7 @@ function App() {
           {/* <Route exact path="/login">aca va el componente de login/inicio de sesion</Route> */}
         </Switch>
       </AuthProvider>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
