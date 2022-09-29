a2c40bb0806e7af513991800f8332a0d2283753f
Angelo <rideralucar@gmail.com>
2022-09-29 14:45:38 -0500

    Implementacion de Page productos

--- client/src/components/ComponentProducts/ComponentProducts.jsx
+++ client/src/components/ComponentProducts/ComponentProducts.jsx
@@ -3,21 +3,22 @@ import Filter from "../Filter/Filter.jsx";
 import FilterDetail from "../FilterDetail/FilterDetail.jsx";
 import Paginated from "../Paginated/Paginated.jsx";
 import s from "./ComponentProducts.module.css"
-
+import styleFilter from "./Filter.module.css";
 import stylePaginated from "./Paginated.module.css";
 import styleCards from "./ProductCards.module.css";
+import styleCard from "./ProductCard.module.css";
 import styleFilterDetail from "./FilterDetail.module.css";
 
 export const ComponentProducts = () => {
     return (
         <div className={s.containerProductsGlobal}>
             <div>
-            <Filter/>
+            <Filter styleFilter={styleFilter}/>
             </div>
             <div className={s.containerProductsGlobal2}>
             <FilterDetail styleFilterDetail={styleFilterDetail}/>
             <Paginated stylePaginated={stylePaginated}/>
-            <ProductCards/>
+            <ProductCards styleCards={styleCards} styleCard={styleCard}/>
             <Paginated stylePaginated={stylePaginated} />
             </div>
         </div>
