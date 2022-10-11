326cdeed814789fe218c78a31e595080e62dd3ac
Richardd82 <richardd82@gmail.com>
2022-10-10 20:38:29 -0500

    Fix Auth 2

--- api/src/routes/authRoutes.js
+++ api/src/routes/authRoutes.js
@@ -33,7 +33,7 @@ console.log("usuario de logueo", user)
 
   const userForToken = user;
 
-  const token = jwt.sign(userForToken, process.env.JWT_secret_key);
+  const token = jwt.sign(userForToken.toJSON(), process.env.JWT_secret_key);
 
   (user.verify === false)
     ? res.redirect("/register").json({ error: "Usuario no verificado" })
@@ -189,9 +189,17 @@ console.log("usuario de logueo", user)
     
       console.log(userValidate)
 
-       const userForToken = user;
+       const userForToken = {
+        username: "username",
+        name: userValidate[0].dataValues.name,
+        lastName: userValidate[0].dataValues.lastName,
+        image: userValidate[0].dataValues.image,
+        address: userValidate[0].dataValues.address,
+        isAdmin: userValidate[0].dataValues.isAdmin,
+        id: userValidate[0].dataValues.id,
+       }
 
-       const token = jwt.sign(userForToken.toJSON(), process.env.JWT_secret_key);
+       const token = jwt.sign(userForToken, process.env.JWT_secret_key);
 
        res.status(200).json({ userForToken, token })
     } catch (err) {
