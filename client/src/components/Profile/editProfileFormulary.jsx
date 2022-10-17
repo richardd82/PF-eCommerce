dcc75ef9b8703403ad1cc5b75b1f336632642a9e
Leandro Ariel Bustamante <96198505+Lean-182@users.noreply.github.com>
2022-10-17 12:16:56 -0300

    ya se encuentra funcional el edit

--- client/src/components/Profile/editProfileFormulary.jsx
+++ client/src/components/Profile/editProfileFormulary.jsx
@@ -1,33 +1,56 @@
 import React, { useEffect, useState } from "react";
 import style from "./Profile.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteUsers, getAllUsers } from "../../redux/actions";
+import { deleteUsers, getAllUsers, putUser } from "../../redux/actions";
 
 
 
 const EditProfileFormulary = () => {
 
 
-    const { editMode, setEditMode } = useState();
+
     const allUsers = useSelector((state) => state.allUsers);
     const dispatch = useDispatch();
     const users = useSelector((state) => state.allUsers);
     const user_login = useSelector((state) => state.user_login);
-
-
+    const [input, setInput] = useState({
+        email: "",
+        name: "",
+        lastName: "",
+        image: "",
+        address: "",
+        phone: null
+    });
+    console.log(user_login)
     useEffect(() => {
         dispatch(getAllUsers())
         dispatch(deleteUsers())
     }, [dispatch]);
 
     const userConnected = users.find(user => user.id === user_login.id)
+    const id = user_login.id;
 
     console.log(userConnected);
 
-    const onSubmit = () => {
-        e.preventDefault();
-        
+    function handleChange(e) {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+        // setErrors(
+        //     validate({
+        //         ...input,
+        //         [e.target.name]: e.target.value,
+        //     })
+        // )
     }
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(input)
+        dispatch(putUser(input, id))
+        alert("usuario modificado")
+    }
+
 
     return (
 
@@ -37,29 +60,42 @@ const EditProfileFormulary = () => {
                     <form>
                         <div >
                             <input type="text" className="form-control"
-                             placeholder="Name" name="userConnected.name" />
+                             placeholder="Name" name="name"
+                             onChange={(e) => handleChange(e)}
+                              />
+                              
                         </div>
                         <div >
                             <input type="text" className="form-control"
-                             placeholder="Lastname" name="userConnected.Lastname" />
+                             placeholder="Lastname" name="lastName"
+                             onChange={(e) => handleChange(e)}
+                             />
                         </div>
                         <div>
                             <input type="email" className="form-control"
-                             placeholder="Email Address" name="userConnected.email" />
+                             placeholder="Email Address" name="email" 
+                             onChange={(e) => handleChange(e)}
+                            />
                         </div>
                         <div>
                             <input type="address" className="form-control"
-                             placeholder="Address" name="userConnected.address" />
+                             placeholder="Address" name="address" 
+                             onChange={(e) => handleChange(e)}
+                            />
                         </div>
                         <div>
                             <input type="string" className="form-control"
-                             placeholder="image" name="userConnected.image" />
+                             placeholder="image" name="image" 
+                             onChange={(e) => handleChange(e)}
+                            />
                         </div>
                         <div>
                             <input type="number" className="form-control"
-                             placeholder="phone" name="userConnected.phone" />
+                             placeholder="phone" name="phone" 
+                             onChange={(e) => handleChange(e)}
+                            />
                         </div>
-                        <button>Agregar o cambiar datos</button>
+                        <button onClick={(e) => handleSubmit(e)}>Agregar o cambiar datos</button>
                     </form>
                 </div>
             ) : (
