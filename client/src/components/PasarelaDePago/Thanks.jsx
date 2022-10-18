bee4bc35b918970fa88b50bcddb266c99eebdb57
Matias <matimogica@gmail.com>
2022-10-18 18:39:42 -0300

    envio de emails y estilo en finalizar pago

--- client/src/components/PasarelaDePago/Thanks.jsx
+++ client/src/components/PasarelaDePago/Thanks.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom" 
 import "./Thanks.css"; 
 
 export class Thanks extends Component {
     render() {
         return (
             <div >
-                <label>
-                    THANKS FOR BUY
-                </label>
-                <a href="/">
-                    <button >Back to home</button>
-                </a>
+                <h2 className="thanksTitle">
+                    THANKS FOR BUY!!
+                </h2>
+                <img className="thanksImg" src="https://cdn.dribbble.com/users/3502921/screenshots/11467047/media/899b3e48a1a7cb78970b54b372db5349.gif" />
+                <Link to="/" className="thanksLink">
+                <button className="thanksBtn" >Return home </button> 
+                </Link>
             </div>
         );
     }
