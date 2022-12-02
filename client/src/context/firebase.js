3a1b493d23ef3bd5a2a4f767e0b2d06d7c824703
Richardd82 <richardd82@gmail.com>
2022-12-02 15:01:31 -0600

    change credentials 4

--- client/src/context/firebase.js
+++ client/src/context/firebase.js
@@ -1,6 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCwKuosJCtBHudenrTG1c2sqQN7IF8mRFE",
   authDomain: "alien-8f953.firebaseapp.com",
@@ -9,15 +14,8 @@ const firebaseConfig = {
   messagingSenderId: "92777818540",
   appId: "1:92777818540:web:4f6bc8c34919a808d5f209",
   measurementId: "G-Y1DECS6BXX"
-  // apiKey: "AIzaSyBZa300JkZwPhH3ZTsUPRyPt62MJLThXi8",
-  // authDomain: "e-commerce-44c1a.firebaseapp.com",
-  // projectId: "e-commerce-44c1a",
-  // storageBucket: "e-commerce-44c1a.appspot.com",
-  // messagingSenderId: "769513353737",
-  // appId: "1:769513353737:web:5a5fce23fc490135a7bed8",
-  // measurementId: "G-0HV2R5042L"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,8 +1,11 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from "firebase/auth"; 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCwKuosJCtBHudenrTG1c2sqQN7IF8mRFE",
   authDomain: "alien-8f953.firebaseapp.com",
@@ -11,14 +14,8 @@ const firebaseConfig = {
   messagingSenderId: "92777818540",
   appId: "1:92777818540:web:4f6bc8c34919a808d5f209",
   measurementId: "G-Y1DECS6BXX"
-  };
+};
 
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-  //EXPORT
-
-  export const auth = getAuth(app)
-
-  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
