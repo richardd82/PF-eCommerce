280db0195dd309702021b93cb60d31c85302d6be
Richardd82 <richardd82@gmail.com>
2022-12-02 14:04:45 -0600

    change credentials

--- client/src/context/firebase.js
+++ client/src/context/firebase.js
@@ -3,13 +3,21 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBZa300JkZwPhH3ZTsUPRyPt62MJLThXi8",
-  authDomain: "e-commerce-44c1a.firebaseapp.com",
+  authDomain: "alienstreet-816f2.firebaseapp.com",
   projectId: "e-commerce-44c1a",
   storageBucket: "e-commerce-44c1a.appspot.com",
   messagingSenderId: "769513353737",
   appId: "1:769513353737:web:5a5fce23fc490135a7bed8",
   measurementId: "G-0HV2R5042L"
+  // apiKey: "AIzaSyBZa300JkZwPhH3ZTsUPRyPt62MJLThXi8",
+  // authDomain: "e-commerce-44c1a.firebaseapp.com",
+  // projectId: "e-commerce-44c1a",
+  // storageBucket: "e-commerce-44c1a.appspot.com",
+  // messagingSenderId: "769513353737",
+  // appId: "1:769513353737:web:5a5fce23fc490135a7bed8",
+  // measurementId: "G-0HV2R5042L"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
